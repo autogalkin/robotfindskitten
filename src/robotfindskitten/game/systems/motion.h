7a28a77a1192ed509c2dde7f3145df3289e59c68
autogalkin <galigorrom@gmail.com>
2023-10-21 02:38:19 +0400

    docs: documents for motion systems

--- src/robotfindskitten/game/systems/motion.h
+++ src/robotfindskitten/game/systems/motion.h
@@ -1,3 +1,7 @@
+/**
+ * @file
+ * @brief The systems for moving actors in the game
+ */
 
 #ifndef _CPP_PROJECTS_ROBOTFINDSKITTEN_SRC_ROBOTFINDSKITTEN_GAME_ECS_PROCESSORS_MOTION_H
 #define _CPP_PROJECTS_ROBOTFINDSKITTEN_SRC_ROBOTFINDSKITTEN_GAME_ECS_PROCESSORS_MOTION_H
@@ -7,12 +11,36 @@
 #include "engine/details/base_types.hpp"
 #include "engine/time.h"
 
+/**
+ * @struct uniform_movement_tag
+ * @brief A tag for movement without pseudo physics.
+ */
 struct uniform_movement_tag {};
+/**
+ * @struct non_uniform_movement_tag
+ * @brief A tag for movement with a pseudo friction.
+ */
 struct non_uniform_movement_tag {};
 
+/**
+ * @struct velocity
+ * @brief Acs as a force. Applies to the translation in the every tick.
+ *
+ * Inside the uniform_motion system, the velocity resets to 0 every tick, and
+ * the entity should set a new velocity afterward
+ * Within the non_uniform_motion system, the velocity slowly decreases to 0 due
+ * to pseudo-friction
+ */
 struct velocity {
     loc v{0};
 };
+
+/**
+ * @typedef translation
+ * @brief Accumullates all movement from all systems and then applies to the
+ * entity's location in the \ref drawing::redrawer after erasing a previous
+ * entity sprite position from the game buffer
+ */
 using translation = dirty_flag<loc>;
 
 namespace motion {
