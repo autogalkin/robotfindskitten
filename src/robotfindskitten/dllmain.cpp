c1cbf8c20d3a08f8555d2ce3c7cdde5007d7ae5a
autogalkin <galigorrom@gmail.com>
2023-10-22 01:45:45 +0400

    refactor: add namespace resolution to functions from Windows.h

--- src/robotfindskitten/dllmain.cpp
+++ src/robotfindskitten/dllmain.cpp
@@ -10,20 +10,20 @@ static constexpr pos GAME_AREA = {150, 100};
 
 namespace console {
 inline std::unique_ptr<void, void (*)(void*)> allocate() {
-    AllocConsole();
+    ::AllocConsole();
     FILE* fdummy = nullptr;
     [[maybe_unused]] auto err = freopen_s(&fdummy, "CONOUT$", "w", stdout);
     err = freopen_s(&fdummy, "CONOUT$", "w", stderr);
     std::cout.clear();
     std::clog.clear();
-    HANDLE hConOut = CreateFileW(L"CONOUT$", GENERIC_READ | GENERIC_WRITE,
+    HANDLE hConOut = ::CreateFileW(L"CONOUT$", GENERIC_READ | GENERIC_WRITE,
                                  FILE_SHARE_READ | FILE_SHARE_WRITE, nullptr,
                                  OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
-    SetStdHandle(STD_OUTPUT_HANDLE, hConOut);
-    SetStdHandle(STD_ERROR_HANDLE, hConOut);
+    ::SetStdHandle(STD_OUTPUT_HANDLE, hConOut);
+    ::SetStdHandle(STD_ERROR_HANDLE, hConOut);
     std::wcout.clear();
     std::wclog.clear();
-    return {nullptr, [](void*) { FreeConsole(); }};
+    return {nullptr, [](void*) { ::FreeConsole(); }};
 }
 } // namespace console
 
@@ -33,11 +33,11 @@ BOOL APIENTRY DllMain(const HMODULE h_module, const DWORD ul_reason_for_call,
 
     if(ul_reason_for_call == DLL_PROCESS_ATTACH) {
         // Ignore thread notifications
-        DisableThreadLibraryCalls(h_module);
+        ::DisableThreadLibraryCalls(h_module);
 
         // Runs the notepad singleton
         notepad& np = notepad::get();
-        auto* const np_module = GetModuleHandleW(
+        auto* const np_module = ::GetModuleHandleW(
             nullptr); // get the module handle of the notepad.exe
 
         [[maybe_unused]] constexpr notepad::opts start_options =
